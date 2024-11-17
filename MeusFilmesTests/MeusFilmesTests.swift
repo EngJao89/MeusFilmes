d274bf3cbf5703552f247e45fb373b41206526cd
EngJao89 <eng_joao@outlook.com>
2024-11-16 22:26:48 -0300

    Initial Commit

---
+++ MeusFilmesTests/MeusFilmesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeusFilmesTests.swift
+//  MeusFilmesTests
+//
+//  Created by João Ricardo Martins Ribeiro on 16/11/24.
+//
+
+import Testing
+@testable import MeusFilmes
+
+struct MeusFilmesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
