56eadbda52364b6c809d5fe6fd65163a04d95cb9
EngJao89 <eng_joao@outlook.com>
2024-11-17 19:46:29 -0300

    feat: implementa lista de filmes.

---
+++ MeusFilmes/File.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  MeusFilmes
+//
+//  Created by João Ricardo Martins Ribeiro on 17/11/24.
+//
+
+import UIKit
+
+class Filme{
+    var titulo: String!
+    var descricao: String!
+    var imagem: UIImage!
+    
+    init(titulo: String, descricao: String) {
+        self.titulo = titulo
+        self.descricao = descricao
+    }
+    
+}
--- MeusFilmes/ViewController.swift
+++ MeusFilmes/ViewController.swift
@@ -7,11 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var filmes: [Filme] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var filme: Filme
+        filme = Filme(titulo: "Filme1", descricao: "Descricao 1")
+        filmes.append(filme)
+        
+        filme = Filme(titulo: "Filme2", descricao: "Descricao 2")
+        filmes.append(filme)
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filmes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let filme: Filme = filmes[ indexPath.row ]
+        let celula = tableView.dequeueReusableCell(withIdentifier: "celula", for: indexPath)
+        celula.textLabel?.text = filme.titulo
+        
+        return celula
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
