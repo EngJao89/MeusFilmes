77f3c9e5aa2e7e5fbd0a900aa062007f27fca0ed
EngJao89 <eng_joao@outlook.com>
2024-11-18 19:57:38 -0300

    feat: implementa detalhes dos filmes.

---
+++ MeusFilmes/DetalhesFilmeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetalhesFilmeViewController.swift
+//  MeusFilmes
+//
+//  Created by João Ricardo Martins Ribeiro on 18/11/24.
+//
+
+import Foundation
+import UIKit
+
+class DetalhesFilmeViewController: UIViewController {
+    
+    @IBOutlet weak var filmeImageView: UIImageView!
+    @IBOutlet weak var tituloLabel: UILabel!
+    @IBOutlet weak var descricaoLabel: UILabel!
+    
+    var filme: Filme!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        filmeImageView.image = filme.imagem
+        tituloLabel.text = filme.titulo
+        descricaoLabel.text = filme.descricao
+        
+    }
+    
+}
+
